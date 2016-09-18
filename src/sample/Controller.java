38acfaa418b7347d7c39368897221c7874b5a4ea
Thomas Schouten <t.m.schouten@student.tue.nl>
2016-09-18 17:33:57 +0200

    made a minimal working design

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,34 +2,62 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+import static com.sun.corba.se.impl.util.Utility.printStackTrace;
 
 public class Controller {
 
     String username = System.getProperty("user.name");
 
     @FXML private Text text;
-    @FXML private Text user;
     @FXML private ImageView yay;
+    @FXML private Button buttonUpdate;
+    @FXML private TextField inputDegrees;
 
-    int count = 0;
+    @FXML protected void buttonUp(ActionEvent event) {
+        sendHttpRequest("http://192.168.2.106/?panel=up");
+    }
 
-    @FXML protected void buttonClick(ActionEvent event){
-        count++;
-        if((count % 2) == 1) {
-            yay.setVisible(true);
-        } else {
-            yay.setVisible(false);
+    @FXML protected void buttonDown(ActionEvent event) {
+        sendHttpRequest("http://192.168.2.106/?panel=down");
+    }
+
+    @FXML protected void buttonAuto(ActionEvent event) {
+        sendHttpRequest("http://192.168.2.106/?panel=auto");
+    }
+
+    @FXML protected void buttonUpdate(ActionEvent event) {
+        sendHttpRequest("http://192.168.2.106/?update");
+        System.out.println(event);
+    }
+
+    @FXML protected void setAngle(ActionEvent event) {
+        sendHttpRequest("http://192.168.2.106/?degrees="+inputDegrees.getText());
+    }
+
+    private void sendHttpRequest(String url) {
+        try {
+            InputStream response = new URL(url).openStream();
+            try (Scanner scanner = new Scanner(response)) {
+                String responseBody = scanner.useDelimiter("\\A").next();
+                text.setText(responseBody);
+            }
+        } catch (IOException e) {
+            printStackTrace();
         }
-        text.setText("Button has been clicked " + count + " times.");
-        if(username.equals("s156767")){
-            user.setText("Hello Thomas!");
-        } else if(username.equals("s152337")) {
-            user.setText("Hello Abby!");
-        } else {
-            user.setText("Hello");
-        }
+
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("SolArduino");
         primaryStage.setScene(new Scene(root, 500, 300));
         primaryStage.show();
     }
